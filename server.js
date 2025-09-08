c070346f349d2f0101b8a37c1e51d64676f5732f
DL-03 <58114228+DL-03@users.noreply.github.com>
2025-09-08 13:35:35 +0300

    New Server Data

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 const apiToken = '7b18cebd6amsh182380e572b9869p19046ejsn752502899000';
-const serverId = '680613';
+const serverId = '1712628';
 const FMCSToken = "SCOPED eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJGcmVlTWNTZXJ2ZXIubmV0Iiwic2NvcGUiOiJVU0VSIiwic2NvcGVSZXN0cmljdGlvbiI6bnVsbCwidXNlcl9pZCI6OTg1ODM4LCJiYXNlX2tleSI6IjhkZTJlZGY3YzYyZTUzZjY1NGNmOGExOGE0ZDAwNjNjIiwiaXNzdWVkX2F0IjoiMjAyNC0xMS0yMyAxNToxOTowMCIsImlzc3VlZF90byI6IjE4LjE4NC4yMTQuMzMifQ.PX43VY5hxVLSzDGH4PiRhgqwFXFERPCIEwZ9vjXp-Jw";
 
 app.use(cors());
